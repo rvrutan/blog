59385c97c067c68c6418d5b4c5b1cf1366b74306
rvrutan <rutanroni@gmail.com>
2024-11-12 22:35:50 -0500

    updated

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -1,51 +1,79 @@
-// TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
-function toggleDarkMode() {
-  const body = document.body;
-  const button = document.getElementById('toggle');
-  body.classList.toggle('dark');
-
-  localStorage.setItem('darkMode', body.classList.contains('dark'));
-
-  if (body.classList.contains('dark')) {
-    button.textContent = '☀️';
-  } else {
-    button.textContent = '☽';
-  }
-}
 const toggleButton = document.getElementById('toggle');
-toggleButton.addEventListener('click', toggleDarkMode);
+let redirectURL = "";
+
+// TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+function applyMode(mode) {
+
+  document.body.classList = mode
+
+}
+
+function toggleMode() {
+
+  const mode = readMode();
+  let nextMode;
+
+  if (mode === 'light') {
+    nextMode = 'dark';
+  } else {
+    nextMode = 'light';
+  }
+
+  applyMode(nextMode);
+
+  saveMode(nextMode)
+}
+
+
+function readMode() {
+  const mode = localStorage.getItem('mode') || "light";
+  return mode;
+}
+
+function saveMode(mode){
+  localStorage.setItem('mode', mode);
+}
+
+
+
+
+
+
+
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
+function readLocalStorage() {
+  const info = localStorage.getItem("formData");
+  const mode = localStorage.getItem("Mode");
+  const parsedInfo = info ? JSON.parse(info) : [];
+  const parsedMode = mode ? JSON.parse(mode) : [];
+  return [parsedInfo, parsedMode];
+}
 
-// function readLocalStorage() {
-// const info = localStorage.getItem('formData');
-// const mode = localStorage.getItem('darkMode');
-// const parsedInfo = info ? JSON.parse(info) : [];
-// const parsedMode = mode ? JSON.parse(mode) : [];
-// return [parsedInfo, parsedMode];
-// }
-
-// const result = readLocalStorage();
-// console.log(result);
+const result = readLocalStorage();
+console.log(result);
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
 
-// function storeLocalStorage() {
-// const existingData = localStorage.getItem('formData');
-// const parsedData = existingData ? JSON.parse(existingData) : [];
-// const updatedData = parsedData.concat(newData);
+function storeLocalStorage(newData) {
+  const [existingInfo, existingMode] = readLocalStorage();
+  if (Array.isArray(newData)) {
+    const updatedInfo = [...existingInfo, ...newData];
+    localStorage.setItem("formData", JSON.stringify(updatedInfo));
+  } else {
+    const updatedInfo = [...existingInfo, ...newData];
+    localStorage.setItem("formData", JSON.stringify(updatedInfo));
+  }
 
-
-// localStorage.setItem('newData' , JSON.stringify(updatedData));
-// return [updatedData, parsedMode];
-// }
+  localStorage.setItem("Mode", JSON.stringify(existingMode));
+}
 
 // ! Use the following function whenever you need to redirect to a different page
-
-let redirectURL = '';
 
 const redirectPage = function (url) {
   redirectURL = url;
   location.assign(url);
-}
+};
+
+toggleButton.addEventListener("click", applyMode);
